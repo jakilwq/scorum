240a3095492705971a0eb231e72ddfc7d0174971
Andrew Masilevich <andrew.masilevich@scorum.com>
2018-06-14 19:14:31 +0300

    Added test

--- tests/chain_tests/budget/top_winners_tests.cpp
+++ tests/chain_tests/budget/top_winners_tests.cpp
@@ -6,6 +6,8 @@
 #include <scorum/chain/services/dev_pool.hpp>
 #include <scorum/chain/services/budget.hpp>
 
+#include <scorum/chain/operation_notification.hpp>
+
 namespace top_winners_tests {
 
 using namespace database_fixture;
@@ -21,10 +23,10 @@ struct top_winners_bundgets_fixture : public database_budget_integration_fixture
     {
         open_database();
 
-        auto amount = dev_pool_service.get().top_budgets_amount;
-        BOOST_REQUIRE_GT(amount, 0u);
+        max_top_amount = dev_pool_service.get().top_budgets_amount;
+        BOOST_REQUIRE_GT(max_top_amount, 0u);
         budget_amount = ASSET_SCR(12e+3);
-        int feed_amount = (budget_amount.amount * (amount + 10)).value;
+        int feed_amount = (budget_amount.amount * (max_top_amount + 10)).value;
 
         actor(initdelegate).create_account(alice);
         actor(initdelegate).give_scr(alice, feed_amount);
@@ -33,7 +35,26 @@ struct top_winners_bundgets_fixture : public database_budget_integration_fixture
         actor(initdelegate).create_account(bob);
         actor(initdelegate).give_scr(bob, feed_amount);
         actor(initdelegate).give_sp(bob, feed_amount);
+
+        auto current_time = dynamic_global_property_service.get().time;
+        auto deadline_step = SCORUM_BLOCK_INTERVAL * 100;
+        alice_deadline_time = current_time + 2 * deadline_step;
+        bob_deadline_time = alice_deadline_time + deadline_step;
+        top_bob_deadline_time = alice_deadline_time - deadline_step;
     }
+
+    void fill_top_with_actor(Actor& actor, const fc::time_point_sec& actor_deadline_time)
+    {
+        for (size_t ci = 0u; ci < max_top_amount; ++ci)
+        {
+            create_advertising_budget(actor, budget_amount, actor_deadline_time).push_in_block();
+        }
+    }
+
+    uint16_t max_top_amount = 0u;
+    fc::time_point_sec alice_deadline_time;
+    fc::time_point_sec bob_deadline_time;
+    fc::time_point_sec top_bob_deadline_time;
 
     asset budget_amount;
 
@@ -45,61 +66,88 @@ struct top_winners_bundgets_fixture : public database_budget_integration_fixture
     budget_service_i& budget_service;
 };
 
+struct advertising_bundgets_visitor
+{
+    typedef void result_type;
+
+    advertising_bundgets_visitor(Actor& checked_actor)
+        : _checked_actor(checked_actor)
+    {
+    }
+
+    void operator()(const allocate_cash_from_advertising_budget_operation& op) const
+    {
+        BOOST_CHECK_EQUAL((std::string)_checked_actor.name, op.owner);
+    }
+
+    template <typename Op> void operator()(Op&&) const
+    {
+    }
+
+private:
+    Actor& _checked_actor;
+};
+
 BOOST_FIXTURE_TEST_SUITE(top_winners_bundgets_tests, top_winners_bundgets_fixture)
 
 BOOST_AUTO_TEST_CASE(get_monopoly_top_budgets_check)
 {
-    auto amount = dev_pool_service.get().top_budgets_amount;
-    auto current_time = dynamic_global_property_service.get().time;
-    auto alice_deadline_time = current_time + SCORUM_BLOCK_INTERVAL * 100;
+    fill_top_with_actor(alice, alice_deadline_time);
 
-    BOOST_REQUIRE_GT(amount, 0u);
-
-    for (size_t ci = 0u; ci < amount; ++ci)
-    {
-        create_advertising_budget(alice, budget_amount, alice_deadline_time).push_in_block();
-    }
-
-    BOOST_REQUIRE_EQUAL(budget_service.get_budgets(alice.name).size(), amount);
-    BOOST_REQUIRE_EQUAL(budget_service.get_top_budgets(amount).size(), amount);
+    BOOST_REQUIRE_EQUAL(budget_service.get_budgets(alice.name).size(), max_top_amount);
+    BOOST_REQUIRE_EQUAL(budget_service.get_top_budgets(max_top_amount).size(), max_top_amount);
 }
 
 BOOST_AUTO_TEST_CASE(get_concurrent_top_budgets_check)
 {
-    auto amount = dev_pool_service.get().top_budgets_amount;
-    auto current_time = dynamic_global_property_service.get().time;
-    auto deadline_step = SCORUM_BLOCK_INTERVAL * 100;
-    auto alice_deadline_time = current_time + 2 * deadline_step;
-    auto bob_deadline_time = alice_deadline_time + deadline_step;
-    auto top_bob_deadline_time = alice_deadline_time - deadline_step;
-
-    BOOST_REQUIRE_GT(amount, 0u);
-
-    for (size_t ci = 0u; ci < amount; ++ci)
-    {
-        create_advertising_budget(alice, budget_amount, alice_deadline_time).push_in_block();
-    }
+    fill_top_with_actor(alice, alice_deadline_time);
 
     create_advertising_budget(bob, budget_amount, bob_deadline_time).push_in_block();
     create_advertising_budget(bob, budget_amount, bob_deadline_time).push_in_block();
 
-    BOOST_REQUIRE_EQUAL(budget_service.get_top_budgets(amount).size(), amount);
+    BOOST_REQUIRE_EQUAL(budget_service.get_top_budgets(max_top_amount).size(), max_top_amount);
 
     // alice is winner
-    for (const budget_object& budget : budget_service.get_top_budgets(amount))
+    for (const budget_object& budget : budget_service.get_top_budgets(max_top_amount))
     {
         BOOST_CHECK_EQUAL((std::string)budget.owner, alice.name);
     }
 
-    for (size_t ci = 0u; ci < amount; ++ci)
-    {
-        create_advertising_budget(bob, budget_amount, top_bob_deadline_time).push_in_block();
-    }
+    fill_top_with_actor(bob, top_bob_deadline_time);
 
     // bob became winner
-    for (const budget_object& budget : budget_service.get_top_budgets(amount))
+    for (const budget_object& budget : budget_service.get_top_budgets(max_top_amount))
     {
         BOOST_CHECK_EQUAL((std::string)budget.owner, bob.name);
+    }
+}
+
+BOOST_AUTO_TEST_CASE(allocation_from_top_budgets_check)
+{
+    {
+        fill_top_with_actor(alice, alice_deadline_time);
+
+        create_advertising_budget(bob, budget_amount, bob_deadline_time).push_in_block();
+        create_advertising_budget(bob, budget_amount, bob_deadline_time).push_in_block();
+
+        // clang-format off
+        boost::signals2::scoped_connection conn = db.post_apply_operation.connect(
+            [&](const operation_notification& note)
+        {
+            note.op.visit(advertising_bundgets_visitor(alice));
+        });
+        // clang-format on
+
+        generate_blocks(5);
+    }
+
+    {
+        fill_top_with_actor(bob, top_bob_deadline_time);
+
+        boost::signals2::scoped_connection conn = db.post_apply_operation.connect(
+            [&](const operation_notification& note) { note.op.visit(advertising_bundgets_visitor(bob)); });
+
+        generate_blocks(5);
     }
 }
 
